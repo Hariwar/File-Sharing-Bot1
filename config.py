8a32405620ab1f440a0d247eb32d2678410193d0
Hariwar <105588706+Hariwar@users.noreply.github.com>
2024-07-19 07:34:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7072700074:AAEAoPtQADIo9U_sbnpL1nIu1v24chJstwA")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7274704597:AAEZwFL9yVd0D180tIQu6lB2zcsmmYy8sLw")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "17822592"))
@@ -19,19 +19,19 @@ APP_ID = int(os.environ.get("APP_ID", "17822592"))
 API_HASH = os.environ.get("API_HASH", "a20b3dbbe07ed695563b4609a3e62012")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002223162495"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002179862255"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "578811855"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1520353976"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Hari813143:hari813143@filestore.wmfcowb.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Cobra:cobra813@cluster0.0lso95o.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "idol")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001767004641"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002065113576"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -39,7 +39,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "578811855").split()):
+    for x in (os.environ.get("ADMINS", "1520353976").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
